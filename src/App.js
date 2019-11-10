3b48a9b5d61eb3501062ea1386a9a740fdad7fa8
tomaszzawada <tomasz.zawada88@gmail.com>
2019-11-10 19:20:59 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="container">                
+            <h1 className="text-center">Top Albums</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
