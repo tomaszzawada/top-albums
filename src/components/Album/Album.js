c8c8e8ff18ea9fb6daa877f635cd48a6266a96da
tomaszzawada <tomasz.zawada88@gmail.com>
2019-11-10 20:28:23 +0100

    added fetching json and albums rendering

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Album = () => {
+const Album = ({title, artist, image}) => {
     return (
          <div className="col-lg-3 col-md-4 col-sm-6">
             <div className="card my-3 justify-content-between">
-                <img src="https://source.unsplash.com/random/200x150" className="card-img-top" alt=""/>
+                <img src={image} className="card-img-top" alt=""/>
                 <div className="text-center pb-3">
-                    <h5 className="card-title">Album Title</h5>
-                    <p className="card-text">Artist Name</p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{artist}</p>
                 </div>
             </div>          
         </div>
--- src/containers/Albums/Albums.js
+++ src/containers/Albums/Albums.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import Album from '../../components/Album/Album';
+import Axios from 'axios';
 
 class Albums extends Component {
+    state = {
+        albums: []
+    }
+
+    componentDidMount(){
+        Axios.get('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
+        .then(res => {
+            this.setState({
+                albums: res.data.feed.entry
+            });
+        })
+        .catch(err => console.log(err));
+    }
+
     render() {
+        const { albums } = this.state;
+        const albumList = albums.length ? (
+            albums.map(album => {
+                return <Album
+                    key={album.id.attributes["im:id"]}
+                    title={album["im:name"].label}
+                    artist={album["im:artist"].label}
+                    image={album["im:image"][2].label}
+                />
+            })
+        ) : (
+            <div className="center">Loading</div>
+        );
+
         return (
             <section className="albums">
                 <div className="container my-5">
                     <div className="row">
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
-                        <Album />
+                        { albumList }
                     </div>
                 </div>
             </section>
