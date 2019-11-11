079275950cae4197095cabe2d4bd5bcd31fdf11f
tomaszzawada <tomasz.zawada88@gmail.com>
2019-11-11 02:39:47 +0100

    Updates

---
+++ precache-manifest.7929922baac44c5f5297ceeab98b44c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987632359f526da4a111fe829f82b0a9",
+    "url": "/top-albums/index.html"
+  },
+  {
+    "revision": "b8182a768dcf6c762fde",
+    "url": "/top-albums/static/css/main.383b2ae2.chunk.css"
+  },
+  {
+    "revision": "9da000140bd83f4c7989",
+    "url": "/top-albums/static/js/2.b771153f.chunk.js"
+  },
+  {
+    "revision": "b8182a768dcf6c762fde",
+    "url": "/top-albums/static/js/main.e88410e5.chunk.js"
+  },
+  {
+    "revision": "cf0dd6663855a29a7ef3",
+    "url": "/top-albums/static/js/runtime-main.78b149d4.js"
+  }
+]);
